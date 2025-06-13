8cb046d85a2bc68fe3625483311207f387ebad71
Croky19 <141557212+Croky18@users.noreply.github.com>
2025-06-13 22:35:36 +0200

    Update electrojob.sql

--- SQL/electrojob.sql
+++ SQL/electrojob.sql
@@ -1,4 +1,4 @@
-INSERT INTO jobs (name, label) VALUES ('delivery', 'Pakketbezorger');
+INSERT INTO jobs (name, label) VALUES ('electro', 'electro');
 
 INSERT INTO job_grades (job_name, grade, name, label, salary, skin_male, skin_female)
 
